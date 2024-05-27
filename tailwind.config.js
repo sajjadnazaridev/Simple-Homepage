36f7d09db776142e6511fa9b4b5a295db93418e3
sajjad nazari dev <sajadnazari269@gmail.com>
2024-05-27 22:02:31 +0330

    base files  project

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./**/*.html", "./src/**/*.js", "./styles/**/*.css"],
+  theme: {
+    extend: {
+      colors: {
+        colorDarkBackground: '#111729',
+        colorDarkPrimary: '#223344',
+        colorDarkSecondary: '#909193',
+        colorLightSecondary: '#F2F9FE',
+        colorLightBackground: '#FFFFFF',
+        colorPrimaryAccent: '#2A4DD0',
+        colorSecondaryAccent: '#4CA154',
+      },
+      fontFamily: {
+        DMSans: ["DM Sans", "sans-serif"],
+      },
+    },
+    screens: {
+      sm: "640px",
+      md: "1024px",
+      lg: "1280px",
+    },
+  },
+  plugins: [],
+}
