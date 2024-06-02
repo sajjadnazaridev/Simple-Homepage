2e140815321bc86a82a265b1694647c032748cfa
sajjad nazari dev <sajadnazari269@gmail.com>
2024-06-02 19:19:05 +0330

    update switch theme

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,6 @@
 const htmlDoc = document.querySelector('html');
 const switchThemeCheckBox = document.getElementById('switchThemeCheckBox');
+const switchThemeCheckBoxMobile = document.getElementById('switchThemeCheckBoxMobile');
 const mainIconTag = document.getElementById('main-btn-switch');
 const logoHeader = document.getElementById('logoHeader');
 const icoMenu = document.getElementById('ico-menu');
@@ -8,6 +9,19 @@ const navLink = document.getElementById('navLink');
 let statusTheme = 'light';
 
 switchThemeCheckBox.addEventListener('click', () => {
+    htmlDoc.classList.toggle('dark');
+    if (statusTheme === 'light') {
+        statusTheme = 'dark';
+        mainIconTag.src = './images/Moon_fill.svg';
+        logoHeader.src = './images/alarado-icon-homepage-dark.svg';
+    } else {
+        statusTheme = 'light';
+        mainIconTag.src = './images/Sun_fill.svg';
+        logoHeader.src = './images/alarado-icon-homepage.svg';
+    }
+});
+
+switchThemeCheckBoxMobile.addEventListener('click', () => {
     htmlDoc.classList.toggle('dark');
     if (statusTheme === 'light') {
         statusTheme = 'dark';
